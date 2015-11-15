cc04811646515096ac3fdf92c39cb7a3c8b7d9dd
Guido Bouman <m@guido.vc>
2015-11-15 23:39:22 +0100

    Move actions to their own component.

---
+++ src/components/actions.vue
@@ -0,0 +1,49 @@
+<style lang="stylus" scoped>
+  .actions
+    display flex
+    flex-direction row
+    justify-content space-between
+
+    > *
+      display flex
+
+      .action
+        padding 10px
+        border 0 solid rgba(0,0,0,0.2)
+        transition background-color 200ms ease
+        cursor pointer
+
+        &:hover
+          background-color rgba(0,0,0,0.05)
+
+      &:first-child
+        flex-direction row
+        .action
+          border-right-width 1px
+
+      &:last-child
+        flex-direction row-reverse
+        .action
+          border-left-width 1px
+</style>
+
+<template>
+  <div class="actions">
+    <div class="left">
+      <div class="action">
+        A 1
+      </div>
+      <div class="action">
+        A 2
+      </div>
+    </div>
+    <div class="right">
+      <div class="action">
+        A 1
+      </div>
+      <div class="action">
+        A 2
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -1,37 +1,33 @@
 <style lang="stylus" scoped>
   header
     flex 0 0 auto
-    display flex
     background white
-    box-shadow 0 3px 0 rgba(0,0,0,0.3)
+    box-shadow 0 3px 0 rgba(0,0,0,0.1)
     z-index 1
-    flex-direction row
-    justify-content space-between
     text-transform uppercase
 
-    > *
-      flex 0 1 auto
-      padding 10px
-
-  .slides
-    border-right 1px solid rgba(0,0,0,0.2)
-
   h1
-    margin 0
+    margin 10px 0
+    position absolute
+    left 50%
+    transform translateX(-50%)
     font-size inherit
-
-  .actions
-    border-left 1px solid rgba(0,0,0,0.2)
 </style>
 
 <template>
-    <header>
-      <div class="slides">
-        Slides
-      </div>
-      <h1>prsnt.co</h1>
-      <div class="actions">
-        Actions
-      </div>
-    </header>
+  <header>
+    <h1>prsnt.co</h1>
+    <actions></actions>
+  </header>
 </template>
+
+
+<script>
+  import Actions from './actions.vue'
+
+  export default {
+    components: {
+      Actions
+    }
+  }
+</script>
