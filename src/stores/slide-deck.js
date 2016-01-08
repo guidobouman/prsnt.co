85038c35b48b022c4f3cc6e50bd1d79ca5cdf54f
Guido Bouman <m@guido.vc>
2016-01-08 01:11:10 +0100

    Fix slide deck store slide adding & - activation.

--- src/stores/slide-deck.js
+++ src/stores/slide-deck.js
@@ -1,13 +1,32 @@
 export default {
   state: {
     slides: [],
-    activeSlideId: 0,
-    activeSlide: {},
+    activeSlideId: null,
+    activeSlide: null,
   },
-  activateSlide: function activateSlide(slideId) {
-    if (this.state.slides[slideId]) {
-      this.state.activeSlideId = slideId;
-      this.state.activeSlide = this.state.slides[slideId];
+  activateSlide(uuid) {
+    const slide = this.state.slides.find(slide => {
+      return slide._uuid == uuid;
+    });
+
+    if(slide) {
+      this.state.activeSlideId = uuid;
+      this.state.activeSlide = slide;
+      return slide;
     }
+
+    return undefined;
+  },
+  addSlide(text) {
+    return this.state.slides.push({
+      _uuid: this._uuid(),
+      text: text || ''
+    })
+  },
+  _uuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
   },
 };
