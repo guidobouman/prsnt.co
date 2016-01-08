42ff1d881e35d4aa2e24f8a4a45b0ba33206a878
Guido Bouman <m@guido.vc>
2016-01-08 01:10:09 +0100

    Remove silly references.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import Vue from 'vue';
 import App from './app.vue';
-import Slide from './stores/slide.js';
-import SlideDeck from './stores/slide-deck.js';
 
 new Vue({
-  data: {
-    Slide,
-    SlideDeck,
-  },
   el: 'body',
   components: {
     App,
